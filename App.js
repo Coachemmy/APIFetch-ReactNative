e1153ea5f4f4a0fe8608667c3978f3ba87b9c430
coachemmy <coachemmyb@gmail.com>
2023-02-08 11:19:08 +0100

    apiCommit

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { Appbar, Card, Paragraph, Searchbar } from 'react-native-paper'
+import { ScrollView } from 'react-native-web'
 
-export default function App() {
+const App = () => {
+
+  const [meals, setMeals] = useState([])
+  const [searchQuery, setSearchQuery] = useState('')
+  console.log(meals)
+  const url = 'https://www.themealdb.com/api/json/v1/1/categories.php'
+
+  const eatMeals = async function(){
+    const response = await fetch(url)
+    const data = await response.json()
+    setMeals(data.categories)
+  } 
+
+  useEffect(()=>{
+    eatMeals()
+  }, [])
+
+  const handleQuery = query =>{
+    setSearchQuery(query)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Appbar style={styles.appBarStyle}>
+        <Appbar.Content title= 'Recipe App' color='white'/>        
+      </Appbar>
+      <Searchbar
+        placeholder='search here'
+        value={searchQuery}
+        onChangeText={handleQuery}
+      />
+      <ScrollView>
+        {
+          meals.map((meal)=>(
+            <Card key={meal.idCategory} style={styles.fitWell}>
+              <Card.Cover source={meal.strCategoryThumb}/>
+              <Card.Title title ={meal.strCategory} />
+              <Card.Content>
+                <Paragraph>{meal.strCategoryDescription}</Paragraph>
+              </Card.Content>
+            </Card>
+          ))
+        }
+      </ScrollView>    
     </View>
-  );
+  )
 }
 
+export default App
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appBarStyle:{
+    backgroundColor: 'purple'
   },
-});
+
+  fitWell:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
